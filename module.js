0a6f071d293ab5c3c6a43c989a2d726bf09e9503
Jose Rodriguez <131327988+RodriguezJose92@users.noreply.github.com>
2023-12-26 11:41:03 -0500

    Update module.js

--- module.js
+++ module.js
@@ -298,7 +298,7 @@ async function serverData ({
   setTimeout(()=>{
     MudiExperience({
       tokenApi:'LXhG4UEYLjdz5fiN3AUr',
-      skuNumber:'Prubaelectrojaponesa',
+      skuNumber:document.querySelector('.value-field.item').innerHTML,
       idCompanyMudi:'0',
       color:'#d73e46',
       containerBtns:document.querySelector('.zoomPad'),
@@ -306,5 +306,3 @@ async function serverData ({
     });
   
   },6000)
-
-console.log('probandoCDN')
